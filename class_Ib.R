c45dac5563563c16e5efc9fc9950f7e7139277ea
utuocha <zinnycp@gmail.com>
2025-08-09 06:04:18 +0100

    Add files via upload

---
+++ class_Ib.R
@@ -0,0 +1,37 @@
+getwd()
+
+dir.create("raw_data")
+dir.create("clean_data")
+dir.create("scripts")
+dir.create("results or Tasks")
+dir.create("plots etc")
+
+data <- read.csv(file.choose())
+
+View(data)
+
+str(data)
+
+#gender, diagnosis and smoker have incorrect or inconsistent data types.
+
+data$gender_fac <- as.factor(data$gender)
+class(data$gender_fac)
+
+data$diagnosis_fac <- as.factor(data$diagnosis)
+class(data$diagnosis_fac)
+
+data$smoker_fac <- as.factor(data$smoker)
+class(data$smoker_fac)
+
+data$smoker_num <- factor(data$smoker_fac,
+                          levels = c("Yes", "No"),
+                          labels = c(0, 1, 2))
+
+class(data$smoker_fac)
+class(data$smoker)
+
+data$smoker_num <- factor(data$smoker_fac, levels = c("Yes", "No"), labels = c(1, 0))
+
+write.csv(data, file = "results or Tasks/patient_info_clean.csv")
+
+save.image(file = "EzinneJanefrancesNwonu_ClassIb_Assignment.RData")
